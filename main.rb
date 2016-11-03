fd8bc80b60e6cf7283bc245612c2639a4f515ae4
Brian Pick <brianr.pick@gmail.com>
2016-11-02 19:35:46 -0700

    fixed the 'store_items.rb' name and added a Namespace to store_items

--- main.rb
+++ main.rb
@@ -2,9 +2,9 @@ require "./store_items.rb"
 require "./food.rb"
 
 
-pants = Store_items.new(color: "Black", price: 10, area: "Clothing")
-rain_jacket = Store_items.new(color: "Green", price: 50, area: "Clothing")
-camping_gear = Store_items.new(color: "Purple", price: 150, area: "Sporting Goods")
+pants = Actualize::Store_items.new(color: "Black", price: 10, area: "Clothing")
+rain_jacket = Actualize::Store_items.new(color: "Green", price: 50, area: "Clothing")
+camping_gear = Actualize::Store_items.new(color: "Purple", price: 150, area: "Sporting Goods")
 
 puts pants.info
 puts rain_jacket.info
